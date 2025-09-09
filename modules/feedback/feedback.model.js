29387bb84dc5e31385ea172950e8c71b89954294
MohamedElsayed002 <mohamed.madoo2016@gmail.com>
2025-09-09 07:03:12 +0300

    modify feedback

--- modules/feedback/feedback.model.js
+++ modules/feedback/feedback.model.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose');
 
 const feedbackSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  wash: { type: mongoose.Schema.Types.ObjectId, ref: 'Wash', required: true },
-  washingPlace: { type: mongoose.Schema.Types.ObjectId, ref: 'WashingPlace', required: true },
-  rating: { type: Number, min: 1, max: 5, required: true },
-  comment: { type: String },
-  complaint: { type: String }, // optional complaint field
-  photo: { type: String }, // URL or base64 string for car photo after wash
+  branchRating: { type: Number, min: 1, max: 5, required: true },
+  employeeRating: { type: Number, min: 1, max: 5, required: true },
+  ratingComment: { type: String }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Feedback', feedbackSchema); 
